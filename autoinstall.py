a3aa82c399d64780f922d87514454980ba309f4e
adobe-kano <yuki.kano@adobe.com>
2020-08-26 08:49:44 +0900

    プロトタイプ作成

---
+++ autoinstall.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import json
+import subprocess
+from argparse import ArgumentParser
+
+def fetch_env() :
+    with open("env.json") as f :
+        return json.load(f)
+
+def main() :
+    env = fetch_env()
+    user_pass = env["user"] + ":" + env["pass"]
+    package_paths = [
+        str(x).replace("\\","/") for x in Path(".").glob(env["path"] + "/*.zip")
+    ]
+    service_jsp = env["domain"] + "/crx/packmgr/service.jsp"
+    for path in package_paths :
+        command = ["curl", "-k", "-sS", "-u", user_pass, "-F", "force=true", "-F", "file=@" + path, "-F", "install=true", service_jsp]
+        subprocess.run(command,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+if __name__ == "__main__" :
+    main()
